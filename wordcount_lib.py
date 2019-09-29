3faa22710408bd3bb327d00cae71b9c0ef2dbbe0
Victor Rodrigues <victor.meca2015@gmail.com>
2019-09-29 14:15:23 -0300

    Initial commit

---
+++ wordcount_lib.py
@@ -0,0 +1,17 @@
+def consume(filename):
+    chars = 0
+    words = 0
+    lines = 0
+
+    with open(filename, 'rt') as fp:
+        for line in fp:
+            lines += 1
+            words += len(line.strip().split())
+            chars += len(line)
+
+    return chars, words, lines
+
+def daaaangerous(param=0):
+    print("I'm the most dangerous function West of the Mississippi, no test "\
+          "will cover me!")
+    return 3 / param
